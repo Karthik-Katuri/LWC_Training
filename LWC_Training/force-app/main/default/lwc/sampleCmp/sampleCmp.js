aea30fa7f358bd3b6c7ddd6c28dec38ad4aae107
Katuri Karthik <kkaturi2003@gmail.com>
2026-08-12 19:08:54 +0530

    render conditionally

--- LWC_Training/force-app/main/default/lwc/sampleCmp/sampleCmp.js
+++ LWC_Training/force-app/main/default/lwc/sampleCmp/sampleCmp.js
@@ -2,6 +2,8 @@ import { LightningElement } from 'lwc';
 
 export default class SampleCmp extends LightningElement {
     greetCmp = "LWC";
+    showWelcomeMessage = true;
+    showSecondMessage = false;
 
     handleMessage(event){
         this.greetCmp = event.target.value;
@@ -10,5 +12,13 @@ export default class SampleCmp extends LightningElement {
         
         const value = this.template.querySelector('lightning-input').value;
         this.greetCmp = value;
+    }
+    handleNext(){
+        this.showWelcomeMessage = false;
+        this.showSecondMessage = true;
+    }
+    handleFinal(){
+        this.showWelcomeMessage = false;
+        this.showSecondMessage = false;
     }
 }
