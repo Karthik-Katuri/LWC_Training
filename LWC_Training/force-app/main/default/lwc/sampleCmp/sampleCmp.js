ffcdae47754e6cfa2743f0bb45abc5df2f8cf812
Katuri Karthik <kkaturi2003@gmail.com>
2026-08-13 16:12:48 +0530

    render list

--- LWC_Training/force-app/main/default/lwc/sampleCmp/sampleCmp.js
+++ LWC_Training/force-app/main/default/lwc/sampleCmp/sampleCmp.js
@@ -8,6 +8,12 @@ export default class SampleCmp extends LightningElement {
     handleMessage(event){
         this.greetCmp = event.target.value;
     }
+    studentsList=[{"name":"John","rollno":1,"age":20,"grade":"A"},
+    {"name":"Alice","rollno":2,"age":19,"grade":"B"},
+    {"name":"Bob","rollno":3,"age":21,"grade":"C"},
+    {"name":"Eve","rollno":4,"age":22,"grade":"A"},
+    {"name":"Charlie","rollno":5,"age":20,"grade":"B"}
+    ]
     handleMessageChange(event){
         
         const value = this.template.querySelector('lightning-input').value;
