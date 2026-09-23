6052fa2c1e10ebea14d08a1809f2b7474ce04e80
Katuri Karthik <kkaturi2003@gmail.com>
2026-09-23 14:05:05 +0530

    record edit form

--- LWC_Training/force-app/main/default/lwc/accessingSFData/accessingSFData.js
+++ LWC_Training/force-app/main/default/lwc/accessingSFData/accessingSFData.js
@@ -1,6 +1,30 @@
 import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class AccessingSFData extends LightningElement {
     fields = ['Name', 'Phone', 'Website', 'Industry', 'Type'];
-    
+    isTypeChanged = false;
+
+    handleSuccess(event) {
+        const updatedRecordId = event.detail.id;
+        console.log('Record updated successfully. Record ID: ' + updatedRecordId);
+
+        const updateToast = new ShowToastEvent({
+            title: 'Success',
+            message: 'Record updated successfully. Record ID: ' + updatedRecordId,
+            variant: 'success'
+        });
+
+        this.dispatchEvent(updateToast);
+    }
+    handleAccountChangeType(event) {
+        const selectedType = event.target.value;
+        console.log('Selected Account Type: ' + selectedType);
+        if (selectedType === 'Technology Partner' || selectedType === 'Customer - Direct') {
+            this.isTypeChanged = true;
+        }
+        else{
+            this.isTypeChanged = false;
+        }
+    }
 }
